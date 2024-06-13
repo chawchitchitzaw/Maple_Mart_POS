9ffdd8e7fca221b8b8a0542dd64bb2bcb6859b9f
Bhone Khant <pkk23899@gmail.com>
2024-06-13 16:11:56 +0630

    itemdetail page adjust

--- src/Screens/Itemdetail.js
+++ src/Screens/Itemdetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, ScrollView, Image, Text, Button,SafeAreaView,FlatList } from 'react-native';
 import chips from '../Assets/chips.png';
 import {
@@ -7,18 +7,20 @@ import {
 } from 'react-native-responsive-screen';
 import Back from '../component/Back/Back';
 
-
+export const data = [
+  {id:1, barcode:'Barcode', name:'1234567891231', price:'1500'},
+  {id:2, barcode:'Category', name:'Drink', price:'1500'},
+  {id:3, barcode:'Description', name:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,', price:'2500'},
+  
+];
 
 const Itemdetail = () => {
-
-  const details = ({ item }) => {
-    <View>
-      
-    </View>
-  };
+  const {product,setProduct}= useState(data);
+  
   return (
     <SafeAreaView>
       <Back lable={'Details'}/>
+      <View>
     <ScrollView style = {styles.container} showsVerticalScrollIndicator={false}>
       <View>
         <View style={styles.imagebox}>
@@ -26,15 +28,20 @@ const Itemdetail = () => {
       </View>
       <View style={styles.info}>
         <Text style={styles.name}>Product Name</Text>
-        <Text style={styles.price}>$99.99</Text>
-      <View>
-        <FlatList
-
-        />
+        <Text style={styles.pricetxt}>aaa</Text>
       </View>
+      <View style={styles.info2}>
+      {data.map((item, index) => (
+        <View key={index} style={styles.flatlistbox}>
+            <Text style={styles.barcodetxt}>{item.barcode}</Text>
+           <Text style={styles.nametxt}>{item.name}</Text>
+        </View>
+    ))}
       </View>
+      
       </View>
     </ScrollView>
+    </View>
     </SafeAreaView>
   );
 };
@@ -42,13 +49,21 @@ const Itemdetail = () => {
 const styles = {
   container: {
     marginTop:wp('2'),
+    marginBottom:wp('2'),
+  },
+  flatlistbox: {
+    flex:3,
+    marginVertical:wp('3'),
+    flexDirection:'row',
+    justifyContent: 'space-between',
+    //height:hp('6'),
     
   },
   imagebox: {
-    
+    flex:1,
     backgroundColor: '#fff',
     width:wp('100'),
-    height:wp('70'),
+    //height:hp('30'),
     alignSelf:'center'
     
   },
@@ -59,22 +74,38 @@ const styles = {
     
   },
   info: {
-    top:-7,
-    marginTop: 20,
+    marginTop: 5,
     marginHorizontal:10,
     backgroundColor:'#fff',
     padding:10,
     borderRadius:5,
   },
+  info2: {
+    flex:1,
+    paddingVertical: 5,
+    marginTop:5,
+    marginBottom:10,
+    backgroundColor:'#fff',
+    paddingHorizontal:15,
+    borderRadius:5,
+  },
   name: {
-    fontSize: 24,
+    fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 10,
+    
+    
   },
-  price: {
+  pricetxt: {
     fontSize: 18,
     color: '#999',
     
+  },
+  barcodetxt: {
+    flex:1,
+  },
+  nametxt: {
+    flex:2,
   },
   description: {
     fontSize: 16,
